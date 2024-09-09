004410b0e8d31ea328fcca7a9eb9f8e321d71202
byoushin <127282072+byoushin@users.noreply.github.com>
2024-09-09 12:20:20 +0900

    add mondai10

---
+++ lib/main10.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animationDouble;
+  final Tween<double> _tweenDouble = Tween(begin: 0.0, end: 200.0);
+  late Animation<Color?> _animationColor;
+  final ColorTween _tweenColor =
+      ColorTween(begin: Colors.green, end: Colors.blue);
+
+  // ボタンタップ時にフラグを切り替える
+  _forward() async {
+    setState(() {
+      _animationController.forward();
+    });
+  }
+
+  _stop() async {
+    setState(() {
+      _animationController.stop();
+    });
+  }
+
+  _reverse() async {
+    setState(() {
+      _animationController.reverse();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    // TweenとAnimationControllerからAnimationを作る(サイズ)
+    _animationDouble = _tweenDouble.animate(_animationController);
+    _animationDouble.addListener(() {
+      setState(() {});
+    });
+    // TweenとAnimationControllerからAnimationを作る(色)
+    _animationColor = _tweenColor.animate(_animationController);
+    _animationColor.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void disporse() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizeTransition(
+                sizeFactor: _animationController,
+                child: Text("福岡情報校",
+                    style: TextStyle(
+                      color: Colors.blue, // 文字色を青に設定
+                      fontSize: 80,
+                    ))),
+            SizeTransition(
+              sizeFactor: _animationController,
+              child: Image.network(
+                'https://www.tsuushinsei.net/shikaku-hiroba/wp-content/uploads/2022/11/tokyojouhou20221124.jpg',
+                width: 1000, // 画像の幅を指定
+                height: 300, // 画像の高さを指定
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        FloatingActionButton(
+            onPressed: _forward, child: const Icon(Icons.arrow_forward)),
+        FloatingActionButton(onPressed: _stop, child: const Icon(Icons.pause)),
+        FloatingActionButton(
+            onPressed: _reverse, child: const Icon(Icons.arrow_back)),
+      ]),
+    );
+  }
+}
